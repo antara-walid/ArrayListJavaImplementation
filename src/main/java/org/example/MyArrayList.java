6a4eb652531b565d8a436d598f54059283cacced
antara-walid <Antara.walid.27@gmail.com>
2022-10-11 20:34:42 +0100

    MyArrayList first virsion

--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,15 +1,87 @@
 package org.example;
 
+import java.io.ObjectInput;
+import java.util.List;
+
 public class MyArrayList<T> {
 
-    private  T [] internalArray ;
+    private Object[] internalArray;
     private int capacity;
 
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+
     public MyArrayList() {
-        internalArray = new
+        internalArray = new Object[DEFAULT_CAPACITY];
     }
 
     public MyArrayList(int capacity) {
         this.capacity = capacity;
+        internalArray = new Object[capacity];
+    }
+
+    // add methode
+    public void add(Object item) {
+        size++;
+        if (size <= capacity) {
+            internalArray[size-1] = item;
+        }
+        else
+        {
+            // placing the internal array values in another array
+            Object[] secondInternalArray = new Object[capacity];
+            for(int i = 0 ; i < capacity ; i++)
+            {
+                secondInternalArray[i] = internalArray[i];
+            }
+
+            // creating new Array whit double the size
+            internalArray = new Object[capacity * 2]; // in this example the resizing factor is 2
+
+            // inserting the old data in the new array
+            for(int i = 0 ; i < capacity ; i++)
+            {
+                 internalArray[i] = secondInternalArray[i];
+            }
+
+            // inserting the item
+            internalArray[size-1] = item;
+        }
+
+    }
+
+    // get methode
+
+    public T get(int index)
+    {
+        return (T) internalArray[index]; // casting
+    }
+
+
+
+    // getters and setters
+
+    public Object[] getInternalArray() {
+        return internalArray;
+    }
+
+    public void setInternalArray(Object[] internalArray) {
+        this.internalArray = internalArray;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 }
