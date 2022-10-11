5d71b5ca1fd2ef5d2fac6d69c03b29e8c6315c5f
antara-walid <Antara.walid.27@gmail.com>
2022-10-11 19:40:13 +0100

    first commit

---
+++ src/main/java/org/example/MyArrayList.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MyArrayList<T> {
+
+    private  T [] internalArray ;
+    private int capacity;
+
+    public MyArrayList() {
+        internalArray = new
+    }
+
+    public MyArrayList(int capacity) {
+        this.capacity = capacity;
+    }
+}
